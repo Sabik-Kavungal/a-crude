bec243ea2faa505ba92dc2466b7386d414dfef40
sabik <sabikkavungal123@gmail.com>
2023-07-03 20:53:29 +0530

    s

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: 'sabik123',
     database: 'images'
 });
 
